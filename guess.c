4758a4509d94e0283c9be5bc2423babe61b4edde
Eugene Kim <eugenekim@cpp.edu>
2022-10-23 16:01:40 -0700

    global $ static var added

--- guess.c
+++ guess.c
@@ -20,25 +20,73 @@
 
 */
 #include <stdio.h> 
+#include <stdlib.h> 
+#include <time.h> 
 
 // Function prototypes
 void displayMenu(); 
 int promptUser();
+int getGuess(); //promt the user and get the guess input
+bool checkGuess(int a, int target); // true if correct guess, false otherwise
+void suggestGuess(int a, int target); // suggest user if input is too low or high
+
+
+// Global and Static variables
+// Static variable max that saves the user defined max, max is 10
+static int Max = 10;
+int guess = 0;
 
 
 int main()
 {
 
-    int input; 
+    //generate RN 1 - 10
+    int target, i; 
+    time_t t;
+
+    srand((unsigned) time(&t));
+    target = (rand()%10) + 1; // it has RN betwen 1 - 10 now. 
+
+
+    int menuInput; 
     bool gameStatus = true; 
 
-    while(gameStatus == true)
-    {
+    do {
+        // NEW GAME STARTS HERE
+        displayMenu();
+        menuInput = promptUser();
 
-        switch(input)
+        // reset the guess at every start of game
+        guess = 0; 
+        bool check = false; 
+        switch(menuInput)
+        {
+            case 1: 
+                // actual game implements here
+                while (check == false)
+                {
+                    // this guess will continue until matching found
+                    guess = getGuess();
+                    check = checkGuess(guess, target);
+                }
+                // out the loop when have correct guess
+
+                break;
+            case 2:
+
+                break; 
+
+            case 3: 
+                printf("Thank you for playing this game! \n");
+                gameStatus = false;
+                break; 
+            default: 
+                printf("Invalid menu input, please pick again...");
+                break;
+        } // end switch
 
 
-    }
+    } while (gameStatus == true);
 
 
 
@@ -51,4 +99,48 @@ void displayMenu()
     printf("Press 2 tp change the max number\n");
     printf("Press 3 to quit\n");
 
+}
+
+int promptUser()
+{
+    int choice; 
+    printf("Please enter your choice: ");
+    scanf("%d", &choice);
+    return choice; 
+
+}
+
+int userGuess()
+{
+    int userInput; 
+    printf("Please enter a number to guess: ");
+    scanf("%d", &userInput);
+
+    return userInput; 
+
+}
+
+bool checkGuess(int a, int target) 
+{
+    if (a == target)
+    { 
+        return true; 
+    }
+    else 
+    {
+        suggestGuess(a, target);
+        return false; 
+    }
+}
+
+void suggestGuess(int a, int target)
+{
+    if(a < target) 
+    {
+        printf("The guess is too low\n");
+    }
+    else
+    {
+        printf("The guess is too high\n"); 
+    }
 }
